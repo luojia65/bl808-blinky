2b4a4c43556d9bb5d74f54b83d1b22fc22b7ddbc
luojia65 <me@luojia.cc>
2022-08-02 22:02:57 +0800

    small fix

--- bl808-blinky-m0.asm
+++ bl808-blinky-m0.asm
@@ -3,20 +3,20 @@ D:\RustSBI\bl808-blinky\target\riscv64imac-unknown-none-elf\release\bl808-blinky
 
 Disassembly of section .text:
 
-0000000050100000 <stext>:
-50100000: 17 81 f3 11  	auipc	sp, 73528
-50100004: 13 01 01 00  	mv	sp, sp
-50100008: 93 02 00 40  	li	t0, 1024
-5010000c: 16 91        	add	sp, sp, t0
-5010000e: 97 00 00 00  	auipc	ra, 0
-50100012: e7 80 20 01  	jalr	18(ra)
-50100016: 97 00 00 00  	auipc	ra, 0
-5010001a: e7 80 c0 00  	jalr	12(ra)
-5010001e: 00 00        	unimp	
+0000000090000000 <stext>:
+90000000: 17 81 03 d2  	auipc	sp, 860216
+90000004: 13 01 01 00  	mv	sp, sp
+90000008: 93 02 00 40  	li	t0, 1024
+9000000c: 16 91        	add	sp, sp, t0
+9000000e: 97 00 00 00  	auipc	ra, 0
+90000012: e7 80 20 01  	jalr	18(ra)
+90000016: 97 00 00 00  	auipc	ra, 0
+9000001a: e7 80 c0 00  	jalr	12(ra)
+9000001e: 00 00        	unimp	
 
-0000000050100020 <_ZN15bl808_blinky_m04main17h0d684832d2af0508E>:
-50100020: 82 80        	ret
+0000000090000020 <_ZN15bl808_blinky_m04main17h0d684832d2af0508E>:
+90000020: 82 80        	ret
 
-0000000050100022 <_ZN15bl808_blinky_m08finalize17h6bb6c7745b35ef1bE>:
-50100022: 0f 00 00 01  	fence	w, unknown
-50100026: f5 bf        	j	0x50100022 <_ZN15bl808_blinky_m08finalize17h6bb6c7745b35ef1bE>
+0000000090000022 <_ZN15bl808_blinky_m08finalize17h6bb6c7745b35ef1bE>:
+90000022: 0f 00 00 01  	fence	w, unknown
+90000026: f5 bf        	j	0x90000022 <_ZN15bl808_blinky_m08finalize17h6bb6c7745b35ef1bE>
